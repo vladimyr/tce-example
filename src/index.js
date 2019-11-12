ca669b0e20d1dabb4a9ea9648f65410eefed42ba
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-12 11:22:22 +0100

    Upgrade `@extensionengine/rollup-plugin-tailor-ce` :wrench:

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import '@/main.scss';
 import Counter from '@/counter';
 import VueSwal from 'vue-swal/dist/vue-swal.common';
 
+export { Counter };
+
 export const install = Vue => {
   Vue.use(VueSwal);
 };
 
 export default {
   initState: () => ({}),
-  Counter
+  components: {
+    Counter
+  }
 };
